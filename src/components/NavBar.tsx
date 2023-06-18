9174fdd9784036aa503d12468cfd05d44fd2ab0b
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-06-18 09:41:03 +0200

    Add logo to navabr

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 
-function NavBar(props) {
+function NavBar() {
     return (
         <nav className="group flex justify-around rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <Link href="/"
+                  className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+                <Image src="/logo.png" alt="Azeno company logo" width="50" height="63" />
+            </Link>
             <Link href="/"
                   className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
                 Home
