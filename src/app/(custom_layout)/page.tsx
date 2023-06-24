cd69a3ba8265c1187312d0112c3e0cede5f869da
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-06-24 11:12:29 +0200

    add sticy navbar

--- src/app/(custom_layout)/page.tsx
+++ src/app/(custom_layout)/page.tsx
@@ -1,7 +1,23 @@
+import About from "@/components/About";
+import Employees from "@/components/Employees";
+import FormContact from "@/components/FormContact";
+
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            Home
+            <section className="flex min-h-screen flex-col items-center justify-between p-24">
+                <h1>Home</h1>
+            </section>
+            <section id="about" className="flex min-h-screen flex-col items-center justify-between p-24">
+                <About/>
+            </section>
+            <section id="employees" className="flex min-h-screen flex-col items-center justify-between p-24">
+                <Employees/>
+            </section>
+            <section id="formContact" className="flex min-h-screen flex-col items-center justify-between p-24">
+                <FormContact/>
+            </section>
+
         </main>
     )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,28 +4,28 @@ import Image from "next/image";
 function NavBar() {
     return (
         <nav
-            className="group flex justify-around rounded-lg border border-transparent px-5 py-4 transition-colors
+            className="sticky top-0 z-50 bg-gray-800 bg-opacity-70 group flex justify-around rounded-lg border border-transparent px-5 py-4 transition-colors
             hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 mb-3">
-            <Link href="/"
+            <a href="/"
                   className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
                 <Image src="/logo.png" alt="Azeno company logo" width="50" height="63"/>
-            </Link>
-            <Link href="/"
+            </a>
+            <a href="/"
                   className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
                 Home
-            </Link>
-            <Link href="/contact"
-                  className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
-                Contact
-            </Link>
-            <Link href="/about"
+            </a>
+            <a href="#about"
                   className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
                 About us
-            </Link>
-            <Link href="/employees"
+            </a>
+            <a href="#employees"
                   className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
                 Employees
-            </Link>
+            </a>
+            <a href="#formContact"
+                  className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+                Contact
+            </a>
         </nav>
     );
 }
