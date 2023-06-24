8f055afa8105afea88cc36f454b84ca71aa90cfb
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-06-24 08:38:01 +0200

    about us page styling

--- src/app/(custom_layout)/about/page.tsx
+++ src/app/(custom_layout)/about/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import apiCall from "@/api/api";
+import {faker} from "@faker-js/faker";
 
 interface Paragraph {
     type: "paragraph";
@@ -22,7 +23,7 @@ interface Content {
 
 function Page() {
     const [content, setContent] = useState<Content | undefined>(undefined);
-    
+
     useEffect(() => {
         apiCall<Content>({endpoint: 'contents/1'})
             .then(setContent)
@@ -31,27 +32,32 @@ function Page() {
     if (content === undefined) {
         return <h2>Loading</h2>
     }
-    
+
     return (
         <>
             <h1 className="text-center mb-3 text-5xl">{content.mainTitle}</h1>
-            <div>
-                <h3>{content.subTitle}</h3>
-                {content.texts.map((text) => (
-                    <>
-                        {text.type === 'paragraph' ? (
-                            <p>{text.content}</p>
-                        ) : (
-                            <ul>
-                                {text.content.map((item) => (
-                                    <li key={item}>
-                                        {item}
-                                    </li>
-                                ))}
-                            </ul>
-                        )}
-                    </>
-                ))}
+            <div className="max-w-5xl m-auto grid grid-cols-2">
+                <div className="col-span-1">
+                    <img src={faker.image.urlPicsumPhotos({width: 500})} alt=""/>
+                </div>
+                <div className="col-span-1">
+                    <h3>{content.subTitle}</h3>
+                    {content.texts.map((text, idx) => (
+                        <React.Fragment key={idx}>
+                            {text.type === 'paragraph' ? (
+                                <p>{text.content}</p>
+                            ) : (
+                                <ul>
+                                    {text.content.map((item) => (
+                                        <li key={item}>
+                                            {item}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </React.Fragment>
+                    ))}
+                </div>
             </div>
         </>
     );
